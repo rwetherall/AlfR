bc753a6cb9a1934e0bb9a9ca71a42cb3a28b1e6a
rwetherall <rwetherall@gmail.com>
2019-05-09 11:05:26 +1000

    Add test framework dependencies
    * add testthat
    * add httptest
    * add alfticket test framework

--- R/alfticket.R
+++ R/alfticket.R
@@ -1,23 +1,25 @@
 require(httr)
 require(magrittr)
 
+endpoint.tickets <- "/alfresco/api/-default-/public/authentication/versions/1/tickets"
+
 ##
 #' @title
-#' TODO
+#' Get Alfresco authentication ticket
 #' @description
-#' TODO
-#' @param uri TODO
-#' @param userid TODO
-#' @param password TODO
-#' @return TODO
+#' Get Alfresco authentication ticket for specified respository with provided credentials.
+#' @param uri base uri to Alfresco repository
+#' @param username user name
+#' @param password password
+#' @return Alfresco ticket that can be used to authenticate subsequent calls to the repository
 #' @export
 ##
-alfticket <- function (uri, userid, password) {
+alfticket <- function (uri, username, password) {
 
   # try to get the authentication ticket for the repository
   response <-
-    paste(uri, "/alfresco/api/-default-/public/authentication/versions/1/tickets", sep = "") %>%
-    POST(body=list(userId = userid, password = password), encode = "json")
+    paste(uri, endpoint.tickets, sep = "") %>%
+    POST(body=list(userId = username, password = password), encode = "json")
 
   if (http_error(response)) {
 
---
+++ tests/testthat/testalfticket.R
@@ -0,0 +1,7 @@
+context("alfticket")
+
+test_that("Given a valid respository, And valid user credentials, When I request a tickt, Then I recieve a valid ticket", {
+    expect_true(TRUE)
+
+  }
+)
