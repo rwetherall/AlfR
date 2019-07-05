d70db517ac53a1774ea7ff6ec14b48cd2f2d5ab6
rwetherall <rwetherall@gmail.com>
2019-07-05 14:29:35 +1000

    Added delete node
    * updated example to be atomic
    * regened docs which now execute the examples correctly
    * added DELETE method to alf http helper methods

--- R/alf_http.R
+++ R/alf_http.R
@@ -2,59 +2,33 @@ require(jsonlite)
 require(magrittr)
 
 ##
-#' @title Alfresco GET method
-#' @description Helper to make a GET call to the Alfresco REST API
-#' @param endpoint base endpoint URI
-#' @param ticket authentication ticket
-#' @param params optional list of parameters
-#' @param as return as \code{json} (default), \code{raw}
-#' @param body request body
-#' @return result based on format provided
-#' @export
+# Helper to make a GET call to the Alfresco REST API
 ##
-alf_GET <- function (endpoint, ticket, params=list(), as=c("json", "raw"), body=NULL)
+alf_GET <- function (endpoint, ticket, params=list(), as=c("json", "raw", "none"), body=NULL)
   alf_method("GET", endpoint, ticket, params, as, body)
 
 ##
-#' @title Alfresco POST method
-#' @description Helper to make a POST call to the Alfresco REST API
-#' @param endpoint base endpoint URI
-#' @param ticket authentication ticket
-#' @param params optional list of parameters
-#' @param as return as \code{json} (default), \code{raw}
-#' @param body request body
-#' @return result based on format provided
-#' @export
+# Helper to make a POST call to the Alfresco REST API
 ##
-alf_POST <- function (endpoint, ticket, params=list(), as=c("json", "raw"), body=NULL)
+alf_POST <- function (endpoint, ticket, params=list(), as=c("json", "raw", "none"), body=NULL)
   alf_method("POST", endpoint, ticket, params, as, body)
 
 ##
-#' @title Alfresco PUT method
-#' @description Helper to make a PUT call to the Alfresco REST API
-#' @param endpoint base endpoint URI
-#' @param ticket authentication ticket
-#' @param params optional list of parameters
-#' @param as return as \code{json} (default), \code{raw}
-#' @param body request body
-#' @return result based on format provided
-#' @export
+# Helper to make a PUT call to the Alfresco REST API
 ##
-alf_PUT <- function (endpoint, ticket, params=list(), as=c("json", "raw"), body=NULL)
+alf_PUT <- function (endpoint, ticket, params=list(), as=c("json", "raw", "none"), body=NULL)
   alf_method("PUT", endpoint, ticket, params, as, body)
 
 ##
-# @title Alfresco HTTP method
-# @description Helper to make a http method call to the Alfresco REST API
-# @param method HTTP method to call
-# @param endpoint base endpoint URI
-# @param ticket authentication ticket
-# @param params optional list of parameters
-# @param as return as \code{json} (default), \code{raw}
-# @param body request body
-# @return result based on format provided
+# Helper to make a DELETE call to the Alfresco REST API
 ##
-alf_method <- function (method=c("GET", "POST", "PUT"), endpoint, ticket, params=list(), as=c("json", "raw"), body=NULL) {
+alf_DELETE <- function (endpoint, ticket, params=list())
+  alf_method("DELETE", endpoint, ticket, params, "none")
+
+##
+# Helper to make a http method call to the Alfresco REST API
+##
+alf_method <- function (method=c("GET", "POST", "PUT", "DELETE"), endpoint, ticket, params=list(), as=c("json", "raw", "none"), body=NULL) {
 
   # check we have a valid method
   method <- match.arg(method)
